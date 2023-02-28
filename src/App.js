38b5a0ab65d2a23e8d54c49bd9eeb91c966f3384
James Aules <james_aules@icloud.com>
2023-02-28 09:55:51 -0500

    Adding helper.js & refactor useState

--- src/App.js
+++ src/App.js
@@ -2,34 +2,35 @@ import { useEffect, useState } from 'react';
 import './App.css';
 import Gallery from './components/Gallery';
 import SearchBar from './components/SearchBar';
-
+import { createResource as fetchData } from './helper';
 function App() {
 
-  let [search, setSearch] = useState('')
-  let [data, setData] = useState([])
+  let [searchTerm, setSearchTerm] = useState('')
+  let [data, setData] = useState(null)
   let [message, setMessage] = useState('Search for Music')
 
   const API_URL = 'https://itunes.apple.com/search?term='
 
   useEffect(() => {
-    if (search) {
-      const fetchData = async () => {
-        document.title = `${search} music`
-        const response = await fetch(API_URL + search)
-        const resData = await response.json()
-        if (resData.results.length > 0) {
-          setData(resData.results)
-        } else {
-          setMessage('Not found')
-        }
-      }
-      fetchData()
+    if (searchTerm) {
+      setData(fetchData(searchTerm))
+      // const fetchData = async () => {
+      //   document.title = `${search} music`
+      //   const response = await fetch(API_URL + search)
+      //   const resData = await response.json()
+      //   if (resData.results.length > 0) {
+      //     setData(resData.results)
+      //   } else {
+      //     setMessage('Not found')
+      //   }
+      // }
+      // fetchData()
     }
-  }, [search])
+  }, [searchTerm])
 
   const handleSearch = (e, term) => {
     e.preventDefault()
-    setSearch(term)
+    setSearchTerm(term)
   }
   return (
     <div className="App">
---
+++ src/helper.js
@@ -0,0 +1,41 @@
+const API_URL = 'https://itunes.apple.com/search?term=';
+
+const fetchSearch = async (searchTerm) => {
+    const response = await fetch(API_URL + searchTerm)
+    const resData = await response.json
+    return resData.results
+}
+// Being that our fetchSearch function returns a promise,
+// We want wrapPromise to take that promise as an argument.
+
+const WrapPromise = (promise) => {
+    let status = 'Pending'
+    let result = ''
+    let suspender = promise.then(responde => {
+        status = 'success'
+        result = responde
+    }, err => {
+        status = 'error'
+        result = err
+        // our suspender represents the resolution of the promise.
+        // An ideal resolution should flag the status to "success"
+    })
+    return {
+
+        read() {
+            if (status === 'Pending') {
+                throw suspender
+            } else if (status === 'error') {
+                throw result
+            }
+            return result
+        }
+    }
+}
+
+export const createResource = (searchTerm) => {
+    return {
+        result: WrapPromise(fetchSearch(searchTerm))
+    }
+}
+WrapPromise 
